10e6f091343a27660f2ee913309b2be77fb4cd73
fabrigonza94 <fabrigonza1994@gmail.com>
2024-01-05 21:34:22 -0300

    Proyecto final JS

--- js/carrito.js
+++ js/carrito.js
@@ -1,52 +1,67 @@
-// Array de productos predefinidos
-/*const productos = [
-    { nombre: `Transportadora`, precio: 1500},
-    { nombre: `Peine para pelos`, precio: 350 },
-    { nombre: `Pelota`, precio: 100 },
-    { nombre: `Surtido de juguetes`, precio: 1000 },
-    { nombre: `Buzo Peludito`, precio: 1000 },
-    { nombre: `Capitas Polares`, precio: 800 },
-    { nombre: `Buzos de Lana`, precio: 1100 },
-    { nombre: `Huesos comestibles medianos`, precio: 200 },
-    { nombre: `Pastillas dog chow`, precio: 1000 },
-    { nombre: `Pastillas frog`, precio: 1500 },
-    { nombre: `Pastillas para gatos`, precio: 1400 },
-    { nombre: `Pastillas para gato adulto`, precio: 900 },
-    { nombre: `Huesos comestibles grandes`, precio: 400 }
-  ];
-
-  // Array para almacenar los productos en el carrito
-  const carrito = [];
-
-  function mostrarCarrito() {
-    if (carrito.length === 0) {
-      alert("El carrito está vacío.");
-    } else {
-      let contenido = "Carrito de Compras:\n\n";
-      let total = 0;
-      carrito.forEach(function(producto) {
-        contenido += "Nombre: " + producto.nombre + "\nPrecio: $" + producto.precio.toFixed(2) + "\n\n";
-        total += producto.precio;
-      });
-      contenido += `Total: $` + total;
-      alert(contenido);
-    }
-  }
-*/
-
- const btnCart = document.querySelector ('.container-cart-icon');
- const prodcutsCarrito = document.querySelector ('.container-cart-product');
-
- btnCart.addEventListener (`click`, () => {
-  prodcutsCarrito.classList.toggle ('hidden-cart')
- });
-  
-
- //lista contenedores de productos
-const products = document.querySelector (`.container-product`);
 
 
-products.addEventListener (`click`, e => {
-    console.log (e)
+const btnCart = document.querySelector(".container-icon");
+const prodcutsCarrito = document.querySelector(".container-cart-product");
+
+btnCart.addEventListener(`click`, () => {
+  prodcutsCarrito.classList.toggle("hidden-cart");
 });
 
+
+let cart = [];
+
+function getCarrito() {
+  cart=JSON.parse(localStorage.getItem('carrito'));
+  carritoHtml();
+};
+
+function agregarCarrito(nombre, precio) {
+  const producto = {
+    nombre: nombre,
+    precio: precio,
+  };
+  cart.push(producto);
+carritoHtml();
+guardarSeccion();
+
+}
+
+function carritoHtml() {
+  let htmlCarito = "";
+  let total = 0;
+  cart.forEach((c) => {
+    htmlCart = `<div class="cart-product">
+                    <div class="info-cart-product">
+                        <div>
+                            <span class="cantidad-producto-carrito">1</span>
+                            <p class="titulo-producto-carrito">${c.nombre}</p>
+                            <span class="precio-producto-carrito">$${c.precio}</span>
+                        </div>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                            stroke-width="1.5" stroke="currentColor" data-slot="icon"
+                            class="icon-close" onclick="eliminar('${c.nombre}')">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M6 18 18 6M6 6l12 12" />
+                        </svg>
+                    </div>
+                </div>`;
+
+    htmlCarito += htmlCart;
+    total = total + Number(c.precio);
+  });
+  document.getElementById("carito").innerHTML = htmlCarito;
+  document.getElementById("pagarTotal").innerHTML = "$" + total.toString();
+  document.getElementById("contador-carrito").innerHTML = cart.length;
+}
+
+function eliminar(nombre) {
+  cart = cart.filter((c) => c.nombre !== nombre);
+  carritoHtml();
+  guardarSeccion();
+}
+
+function guardarSeccion(){
+
+  localStorage.setItem('carrito', JSON.stringify(cart));
+
+}
